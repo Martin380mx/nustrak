5ad466e5ebf54bece276a1570de6f413464fd735
Martin380mx <leyvalopezmartin7@outlook.com>
2026-03-18 19:28:09 -0600

    Modificaciones Generales

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nustrak/splash_screen.dart';
+import 'package:nustrak/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SplashScreen());
+    await tester.pumpWidget(const NustrakApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
